1f2b52e386bf27f91e5042bfaa7b2ca9482218c9
Malte Reißig <Malte Reiig>
2012-08-16 18:12:52 +0200

    revised mathjax setup (with SVG as new math default renderer).
    	- fixed startup issues with embedded mathjax.js in chromium and firefox
    	  while initial, uncritical 404s can still occur if HTML-CSS renderer was selected,
    	  starting the dm4-webclient with embedded mathjax is smooth all over with SVG
    	  note: we use a patched fileURL implementation of MathJax.js 2.0
    	- adjusted project domain name to "de.tu-berlin.eduzen"
    	- custom icons added to excercise_object, excercise and course
    
    uploaded a new bundle ready for deployment into target/-folder

--- src/main/resources/web/script/plugin.js
+++ src/main/resources/web/script/plugin.js
@@ -1,32 +1,59 @@
-dm4c.add_plugin("de.deepamehta.eduzen", function() {
+dm4c.add_plugin("de.tu-berlin.eduzen", function() {
 
-    // -------------------------------------------------------- 
-    dm4c.load_script('/de.deepamehta.eduzen/script/vendor/mathjax/MathJax.js?delayStartupUntil=configured')
-    // 
-    // dm4c.load_script('/de.deepamehta.eduzen/script/vendor/mathjax/extensions/v1.0-warning.js')
+    dm4c.load_script('/de.tu-berlin.eduzen/script/vendor/mathjax/MathJax.js'
+        + '?config=default&delayStartupUntil=configured')
+
+    // note: the following is all because of in chromium these scripts are not automatically loaded, in firefox they are
+    dm4c.load_script('/de.tu-berlin.eduzen/script/vendor/mathjax/extensions/mml2jax.js')
+    dm4c.load_script('/de.tu-berlin.eduzen/script/vendor/mathjax/extensions/tex2jax.js')
+    dm4c.load_script('/de.tu-berlin.eduzen/script/vendor/mathjax/extensions/TeX/noErrors.js')
+    dm4c.load_script('/de.tu-berlin.eduzen/script/vendor/mathjax/extensions/TeX/noUndefined.js')
+    dm4c.load_script('/de.tu-berlin.eduzen/script/vendor/mathjax/extensions/MathEvents.js')
+    dm4c.load_script('/de.tu-berlin.eduzen/script/vendor/mathjax/extensions/MathMenu.js')
+    dm4c.load_script('/de.tu-berlin.eduzen/script/vendor/mathjax/extensions/MathZoom.js')
+    dm4c.load_script('/de.tu-berlin.eduzen/script/vendor/mathjax/extensions/toMathML.js')
+    dm4c.load_script('/de.tu-berlin.eduzen/script/vendor/mathjax/extensions/TeX/AMSmath.js')
+    dm4c.load_script('/de.tu-berlin.eduzen/script/vendor/mathjax/extensions/TeX/AMSsymbols.js')
+
+    dm4c.load_script('/de.tu-berlin.eduzen/script/vendor/mathjax/jax/element/mml/jax.js')
+    dm4c.load_script('/de.tu-berlin.eduzen/script/vendor/mathjax/jax/input/TeX/config.js')
+    dm4c.load_script('/de.tu-berlin.eduzen/script/vendor/mathjax/jax/input/TeX/jax.js')
+    dm4c.load_script('/de.tu-berlin.eduzen/script/vendor/mathjax/jax/input/MathML/config.js')
+    dm4c.load_script('/de.tu-berlin.eduzen/script/vendor/mathjax/jax/output/HTML-CSS/config.js')
+    dm4c.load_script('/de.tu-berlin.eduzen/script/vendor/mathjax/jax/output/HTML-CSS/jax.js')
+    dm4c.load_script('/de.tu-berlin.eduzen/script/vendor/mathjax/jax/output/NativeMML/config.js')
+    // dm4c.load_script('/de.tu-berlin.eduzen/script/vendor/mathjax/jax/output/HTML-CSS/fonts/TeX/fontdata.js')
 
     // define type specific commands and register them
     dm4c.add_listener('init', function (e) {
-        // say hello to
-        console.log("EduZEN-Plugin.initializing(0.1.0-SNAPSHOT) ... ")
         // ### the workaround to load all important mathjax stuff, except the one file commented above^^
-        MathJax.Ajax.config.root = "http://stable.eduzen.tu-berlin.de:8080/de.deepamehta.eduzen/script/vendor/mathjax"
+        // MathJax.Ajax.config.root = "http://localhost:8080/de.tu-berlin.eduzen/script/vendor/mathjax"
+        MathJax.Ajax.config.root = "http://stable.eduzen.tu-berlin.de:8080/de.tu-berlin.eduzen/script/vendor/mathjax"
         console.log("   > new MathJax.Ajax.config.root => " + MathJax.Ajax.config.root) // ff does not know w.l.origin
-        // 
         MathJax.Hub.Config({
-            config: ["MMLorHTML.js"],
-            extensions: ["tex2jax.js","mml2jax.js","MathEvents.js","MathZoom.js","MathMenu.js",
-                "toMathML.js","TeX/noErrors.js","TeX/noUndefined.js","TeX/AMSmath.js","TeX/AMSsymbols.js"],
-            jax: ["input/TeX","input/MathML","output/HTML-CSS","output/NativeMML"],
-            tex2jax: { inlineMath: [["$","$"],["\\(","\\)"]] },
-            menuSettings: {
-                zoom: "Double-Click", mpContext: true, mpMouse: true
+            "extensions": ["tex2jax.js", "mml2jax.js", "MathEvents.js", "MathZoom.js", "MathMenu.js", "toMathML.js",
+               "TeX/noErrors.js","TeX/noUndefined.js","TeX/AMSmath.js","TeX/AMSsymbols.js", "FontWarnings.js"],
+            "jax": ["input/TeX", "output/SVG"], // "input/MathML", "output/HTML-CSS", "output/NativeMML"
+            "tex2jax": { "inlineMath": [["$","$"],["\\(","\\)"]] },
+            "menuSettings": {
+                "zoom": "Double-Click", "mpContext": true, "mpMouse": true, "zscale": "200%", "texHints": true
             },
-            errorSettings: {
-                message: ["[Math Error]"]
-            }
+            "errorSettings": {
+                "message": ["[Math Error]"]
+            },
+            "displayAlign": "left",
+            "SVG": { "blacker": 8, "scale": 110 },
+            "v1.0-compatible": false,
+            "skipStartupTypeset": false,
+            "elements": ["page-content"]
         });
         MathJax.Hub.Configured() // bootstrapes mathjax.js lib
+        // registers a listener for just typesetted dom elements
+        /** MathJax.Hub.Register.MessageHook("New Math", function (message) {
+            if (console != undefined) console.log("MathJax.newmath.. ")
+            var script = MathJax.Hub.getJaxFor(message[1]).SourceElement()
+            if (console != undefined) console.log(message.join(" ")+": '"+script.text+"'")
+        }) **/
         console.log("EduZEN.configuredMathJax().. for further investigation, check out MathJax-object next line.")
         console.log(MathJax) // is here to learn more about mathjax
         // fixme: hide "Create Topic Type" menu item
--- src/main/resources/web/script/renderers/simple_renderers/mathjax_field_renderer.js
+++ src/main/resources/web/script/renderers/simple_renderers/mathjax_field_renderer.js
@@ -1,51 +1,41 @@
-//  MathJax.js (TeX) renderer for "tub.eduzen.excercise_description",
+//  MathJax.js (TeX) simple renderer and single-editor for "tub.eduzen.excercise_description",
 //      "tub.eduzen.excercise_object" and "tub.eduzen.approach.content"
 dm4c.add_simple_renderer('tub.eduzen.mathjax_field_renderer', {
 
     render_info: function(model, $parent) {
-        // 
-        if (typeof(model) == "object") {
-            var value = model.value
-        } else {
-            var value = model
-        }
+
         // render label
         dm4c.render.field_label(model, $parent)
-        var html = '<div class="task content" id="content" style="border: 1px solid #e8e8e8;">'
-            + '<div id="math-output" class="output">' + value + '</div>'
+        var html = '<div class="task content" id="content">'
+            + '<div id="math-output" class="output">' + model.value + '</div>'
             + '</div>'
         $parent.append(html)
-        // interpret just math in div#math-output
-        // MathJax.Hub.Queue(["Typeset", MathJax.Hub, "math-output"])
-        // and/or typeset whole page-panel once
-        MathJax.Hub.Queue(["Typeset", MathJax.Hub, "page-panel"])
-        // --------------- "Update", "Typeset", "Process"
-    },
+
+        // typeset all elements containing TeX to SVG in #page-content
+        MathJax.Hub.Typeset()
+
+        },
 
     render_form: function(model, $parent) {
-        //
-        if (typeof(model) == "object") {
-            var value = model.value
-        } else {
-            var value = model
-        }
-        var elementInputId = 'math-input-' + model.topic.id;
-        var elementOutputId = 'math-output-' + model.topic.id;
+        var elementInputId = 'math-input-' + model.topic.id
+        var elementOutputId = 'math-output-' + model.topic.id
         var $content = $('<textarea id="' + elementInputId +
-            '" type="text" rows="4">').attr({ value : value, size : 80 })
-        //
+            '" type="text" rows="4">').attr({ value : model.value, size : 80 })
+
         $parent.append($content)
-        dm4c.render.field_label("Vorschau", $parent)
-        var html = '<div class="math content preview" id="content" style="border: 1px solid #e8e8e8;">'
-            + '<div id="' + elementOutputId + '" class="output">' + value + '</div>'
+        dm4c.render.field_label("Preview", $parent)
+        var html = '<div id="math-preview" class="math">'
+            + '<div id="' + elementOutputId + '" class="output">' + model.value + '</div>'
             + '</div>'
         $parent.append(html)
-        // register keyup-handler on math-input field
+
+        // register keyup-handler on math-input textarea
         $('#' + elementInputId).keyup(function () {
             $('#' + elementOutputId).text($('#' + elementInputId).val())
             MathJax.Hub.Queue(["Typeset", MathJax.Hub, elementOutputId])
         })
-        // ### TODO: implement proper return function, with usage of dm4c.REF_PREFIX to reference existing ones
+
+        // ### fixme: implement proper return function, with usage of dm4c.REF_PREFIX to reference existing ones
         return function() { // return input value
             return $.trim($content.val())
         }
