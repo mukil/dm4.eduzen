5ff3e3f78897ac7fb70ab0bcf9e3922ecd5e64b3
Willfried van de Hoods <malt@lokal.(none)>
2012-09-25 20:30:30 +0200

    extracted mathjax integration, removed /target-folder.
    updated the mathjax integration accordingly and added some missing icons to the repo.

--- README.md
+++ README.md
@@ -8,11 +8,17 @@ Project Website: <http://web.eduzen.tu-berlin.de>
 
 # Changelog
 
+0.2.0-SNAPSHOT, Sep 25, 2012:
+
+- moved mathjax integration to it`s own plugin <https://github.com/mukil/dm4-mathjax-renderer>
+- meanwhile we made a couple of changes in the model
+- to be upgraded to run with DeepaMehta 4.0.12
+
 0.1.0-SNAPSHOT, Jul 20, 2012:
 
 - TeX based info_renderer <http://www.mathjax.org> introduced to  'dm4.webclient'
 - eduZEN-Application extended about '/eduzen/content/create'-service and application 'content' model
 - eduZEN-Application Model (as of revision 0.3) available for experimental testing with dm4-webclient
 
-Last edited on Jul 20, 2012
+Last edited on Sep 25, 2012
 
--- src/main/resources/web/script/plugin.js
+++ src/main/resources/web/script/plugin.js
@@ -1,34 +1,34 @@
 dm4c.add_plugin("de.tu-berlin.eduzen", function() {
 
-    dm4c.load_script('/de.tu-berlin.eduzen/script/vendor/mathjax/MathJax.js'
+    dm4c.load_script('/de.tu-berlin.eduzen.mathjax-renderer/script/vendor/mathjax/MathJax.js'
         + '?config=default&delayStartupUntil=configured')
 
     // note: the following is all because of in chromium these scripts are not automatically loaded, in firefox they are
-    dm4c.load_script('/de.tu-berlin.eduzen/script/vendor/mathjax/extensions/mml2jax.js')
-    dm4c.load_script('/de.tu-berlin.eduzen/script/vendor/mathjax/extensions/tex2jax.js')
-    dm4c.load_script('/de.tu-berlin.eduzen/script/vendor/mathjax/extensions/TeX/noErrors.js')
-    dm4c.load_script('/de.tu-berlin.eduzen/script/vendor/mathjax/extensions/TeX/noUndefined.js')
-    dm4c.load_script('/de.tu-berlin.eduzen/script/vendor/mathjax/extensions/MathEvents.js')
-    dm4c.load_script('/de.tu-berlin.eduzen/script/vendor/mathjax/extensions/MathMenu.js')
-    dm4c.load_script('/de.tu-berlin.eduzen/script/vendor/mathjax/extensions/MathZoom.js')
-    dm4c.load_script('/de.tu-berlin.eduzen/script/vendor/mathjax/extensions/toMathML.js')
-    dm4c.load_script('/de.tu-berlin.eduzen/script/vendor/mathjax/extensions/TeX/AMSmath.js')
-    dm4c.load_script('/de.tu-berlin.eduzen/script/vendor/mathjax/extensions/TeX/AMSsymbols.js')
+    dm4c.load_script('/de.tu-berlin.eduzen.mathjax-renderer/script/vendor/mathjax/extensions/mml2jax.js')
+    dm4c.load_script('/de.tu-berlin.eduzen.mathjax-renderer/script/vendor/mathjax/extensions/tex2jax.js')
+    dm4c.load_script('/de.tu-berlin.eduzen.mathjax-renderer/script/vendor/mathjax/extensions/TeX/noErrors.js')
+    dm4c.load_script('/de.tu-berlin.eduzen.mathjax-renderer/script/vendor/mathjax/extensions/TeX/noUndefined.js')
+    dm4c.load_script('/de.tu-berlin.eduzen.mathjax-renderer/script/vendor/mathjax/extensions/MathEvents.js')
+    dm4c.load_script('/de.tu-berlin.eduzen.mathjax-renderer/script/vendor/mathjax/extensions/MathMenu.js')
+    dm4c.load_script('/de.tu-berlin.eduzen.mathjax-renderer/script/vendor/mathjax/extensions/MathZoom.js')
+    dm4c.load_script('/de.tu-berlin.eduzen.mathjax-renderer/script/vendor/mathjax/extensions/toMathML.js')
+    dm4c.load_script('/de.tu-berlin.eduzen.mathjax-renderer/script/vendor/mathjax/extensions/TeX/AMSmath.js')
+    dm4c.load_script('/de.tu-berlin.eduzen.mathjax-renderer/script/vendor/mathjax/extensions/TeX/AMSsymbols.js')
 
-    dm4c.load_script('/de.tu-berlin.eduzen/script/vendor/mathjax/jax/element/mml/jax.js')
-    dm4c.load_script('/de.tu-berlin.eduzen/script/vendor/mathjax/jax/input/TeX/config.js')
-    dm4c.load_script('/de.tu-berlin.eduzen/script/vendor/mathjax/jax/input/TeX/jax.js')
-    dm4c.load_script('/de.tu-berlin.eduzen/script/vendor/mathjax/jax/input/MathML/config.js')
-    dm4c.load_script('/de.tu-berlin.eduzen/script/vendor/mathjax/jax/output/HTML-CSS/config.js')
-    dm4c.load_script('/de.tu-berlin.eduzen/script/vendor/mathjax/jax/output/HTML-CSS/jax.js')
-    dm4c.load_script('/de.tu-berlin.eduzen/script/vendor/mathjax/jax/output/NativeMML/config.js')
-    // dm4c.load_script('/de.tu-berlin.eduzen/script/vendor/mathjax/jax/output/HTML-CSS/fonts/TeX/fontdata.js')
+    dm4c.load_script('/de.tu-berlin.eduzen.mathjax-renderer/script/vendor/mathjax/jax/element/mml/jax.js')
+    dm4c.load_script('/de.tu-berlin.eduzen.mathjax-renderer/script/vendor/mathjax/jax/input/TeX/config.js')
+    dm4c.load_script('/de.tu-berlin.eduzen.mathjax-renderer/script/vendor/mathjax/jax/input/TeX/jax.js')
+    dm4c.load_script('/de.tu-berlin.eduzen.mathjax-renderer/script/vendor/mathjax/jax/input/MathML/config.js')
+    dm4c.load_script('/de.tu-berlin.eduzen.mathjax-renderer/script/vendor/mathjax/jax/output/HTML-CSS/config.js')
+    dm4c.load_script('/de.tu-berlin.eduzen.mathjax-renderer/script/vendor/mathjax/jax/output/HTML-CSS/jax.js')
+    dm4c.load_script('/de.tu-berlin.eduzen.mathjax-renderer/script/vendor/mathjax/jax/output/NativeMML/config.js')
+    // dm4c.load_script('/de.tu-berlin.eduzen.mathjax-renderer/script/vendor/mathjax/jax/output/HTML-CSS/fonts/TeX/fontdata.js')
 
     // define type specific commands and register them
     dm4c.add_listener('init', function (e) {
         // ### the workaround to load all important mathjax stuff, except the one file commented above^^
-        // MathJax.Ajax.config.root = "http://localhost:8080/de.tu-berlin.eduzen/script/vendor/mathjax"
-        MathJax.Ajax.config.root = "http://stable.eduzen.tu-berlin.de:8080/de.tu-berlin.eduzen/script/vendor/mathjax"
+        MathJax.Ajax.config.root = "http://localhost:8080/de.tu-berlin.eduzen.mathjax-renderer/script/vendor/mathjax"
+        // MathJax.Ajax.config.root = "http://stable.eduzen.tu-berlin.de:8080/de.tu-berlin.eduzen/script/vendor/mathjax"
         console.log("   > new MathJax.Ajax.config.root => " + MathJax.Ajax.config.root) // ff does not know w.l.origin
         MathJax.Hub.Config({
             "extensions": ["tex2jax.js", "mml2jax.js", "MathEvents.js", "MathZoom.js", "MathMenu.js", "toMathML.js",
