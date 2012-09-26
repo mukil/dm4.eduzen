fc3a1e02bb57ba52570c5333eb7a8eb13eb5e7c1
Willfried van de Hoods <malt@lokal.(none)>
2012-09-26 03:31:34 +0200

    adding migrations 4,5,6 for dm4 2f32c7d1f8b1a2ee7588015fe6fcfc044832d384 to 4.0.12

---
+++ src/main/java/de/deepamehta/plugins/eduzen/migrations/Migration4.java
@@ -0,0 +1,41 @@
+package de.deepamehta.plugins.eduzen.migrations;
+
+import de.deepamehta.core.Topic;
+import de.deepamehta.core.TopicType;
+import de.deepamehta.core.model.SimpleValue;
+import de.deepamehta.core.service.Migration;
+
+
+
+public class Migration4 extends Migration {
+
+    // -------------------------------------------------------------------------------------------------- Public Methods
+
+    @Override
+    public void run() {
+
+        // set new role URIs
+        Topic creatorRole = dms.getTopic("uri", new SimpleValue("dm4.accesscontrol.role_creator"), false, null);
+        creatorRole.setUri("dm4.accesscontrol.user_role.creator");
+        Topic ownerRole = dms.getTopic("uri", new SimpleValue("dm4.accesscontrol.role_owner"), false, null);
+        ownerRole.setUri("dm4.accesscontrol.user_role.owner");
+        Topic memberRole = dms.getTopic("uri", new SimpleValue("dm4.accesscontrol.role_member"), false, null);
+        memberRole.setUri("dm4.accesscontrol.user_role.member");
+        Topic userRole = dms.getTopic("uri", new SimpleValue("dm4.accesscontrol.role_user"), false, null);
+        userRole.setUri("dm4.accesscontrol.user_role.user");
+        Topic everyoneRole = dms.getTopic("uri", new SimpleValue("dm4.accesscontrol.role_everyone"), false, null);
+        everyoneRole.setUri("dm4.accesscontrol.user_role.everyone");
+
+        // set new operation URIs
+        Topic writeOperation = dms.getTopic("uri", new SimpleValue("dm4.accesscontrol.operation_write"), false, null);
+        writeOperation.setUri("dm4.accesscontrol.operation.write");
+        Topic createOperation = dms.getTopic("uri", new SimpleValue("dm4.accesscontrol.operation_create"), false, null);
+        createOperation.setUri("dm4.accesscontrol.operation.create");
+
+        // update topictype 
+        TopicType roleType = dms.getTopicType("dm4.accesscontrol.role", null);
+        roleType.setUri("dm4.accesscontrol.user_role");
+        roleType.setSimpleValue("User Role");
+    }
+
+}
